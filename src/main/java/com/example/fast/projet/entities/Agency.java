c456877e104638890a90a47a3c2cff508e68448b
Abel Ngbandaman <ngbandaman2022@gmail.com>
2023-03-07 13:11:00 +0000

    Creations of API - creation repositories and unit tests : creation properties entities without relations

--- src/main/java/com/example/fast/projet/entities/Agency.java
+++ src/main/java/com/example/fast/projet/entities/Agency.java
@@ -1,5 +1,47 @@
 package com.example.fast.projet.entities;
 
+import java.util.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Agency {
     
+    @Id
+    @SequenceGenerator( // to review
+        name = "agancy_sequence",
+        sequenceName = "agency_sequence",
+        allocationSize = 1
+    )
+    @GeneratedValue( // serves to definie constraints
+        strategy = GenerationType.SEQUENCE,
+        generator = "agency_sequence"
+    )
+    private Long agencyId;
+
+    private String agencyCode;
+
+    private String angencyName;
+
+    private String agencyMobile;
+
+    private String agencyAdress;
+
+    private Date dateCreated;
+
+    private Date dateUpdated;
+
+    private Date dateDeleted;
 }
--- src/main/java/com/example/fast/projet/entities/Queue.java
+++ src/main/java/com/example/fast/projet/entities/Queue.java
@@ -1,5 +1,43 @@
 package com.example.fast.projet.entities;
 
+import java.util.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Queue {
+    @Id
+    @SequenceGenerator( 
+        name = "queue_sequence",
+        sequenceName = "queue_sequence",
+        allocationSize = 1
+    )
+    @GeneratedValue(
+        strategy = GenerationType.SEQUENCE,
+        generator = "queue_sequence"
+    )
+    private Long queueId;
+
+    private String queueCode;
     
+    private Integer queueSize;
+
+    private Date dateCreated;
+
+    private Date dateUpdated;
+
+    private Date dateDeleted;
 }
+
