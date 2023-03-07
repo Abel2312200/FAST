f246c16477fd64b9bc55ff978cdc09b2d36252c0
Abel Ngbandaman <ngbandaman2022@gmail.com>
2023-03-07 08:54:03 +0000

    Creation and initialization

---
+++ src/main/java/com/example/fast/projet/FastProjetApplication.java
@@ -0,0 +1,13 @@
+package com.example.fast.projet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastProjetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastProjetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/fast/projet/FastProjetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fast.projet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastProjetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
